3cf453eba3dbb9a6746d593d8e6d478877e7080c
Alessa42 <psychomatic42@gmail.com>
2025-01-21 17:57:49 +0300

    Init

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,11 @@
 plugins {
+    id("application")
     id("java")
     id("com.github.ben-manes.versions") version "0.50.0"
 
 
 }
-
+application { mainClass.set("hexlet.code.App") }
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
 
