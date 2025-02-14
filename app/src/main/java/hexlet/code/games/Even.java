ee2dec9ed84905141c94c02a80c101a7086e5d11
Alessa42 <psychomatic42@gmail.com>
2025-02-14 18:15:30 +0300

    fixed class Even

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -6,7 +6,6 @@ public class Even {
     public static void startGame() {
         var random = new Random();
         String[][] questionsAndAnswers = new String[Engine.NUMBER_OF_QUESTIONS][2]; // 3 вопроса
-
         for (int i = 0; i < Engine.NUMBER_OF_QUESTIONS; i++) {
             int number = random.nextInt(100); // Генерируем случайное число
             questionsAndAnswers[i][0] = Integer.toString(number);
