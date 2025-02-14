6a7b337d38b308be760325f8bdfc9338814a6991
Alessa42 <psychomatic42@gmail.com>
2025-02-14 18:56:40 +0300

    fixed class Even

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -5,13 +5,13 @@ import hexlet.code.Engine;
 public class Even {
     public static void startGame() {
         var random = new Random();
-        String[][] questionsAndAnswers = new String[Engine.NUMBER_OF_QUESTIONS][2]; // 3 вопроса
+        String[][] questionsAndAnswers = new String[Engine.NUMBER_OF_QUESTIONS][2];
         for (int i = 0; i < Engine.NUMBER_OF_QUESTIONS; i++) {
-            int number = random.nextInt(100); // Генерируем случайное число
+            int number = random.nextInt(100);
             questionsAndAnswers[i][0] = Integer.toString(number);
-            questionsAndAnswers[i][1] = (number % 2 == 0) ? "yes" : "no"; // Проверяем четность
+            questionsAndAnswers[i][1] = (number % 2 == 0) ? "yes" : "no";
         }
-        String gameDescription = "Answer 'yes' if the number is even, otherwise answer 'no'"; // Описание игры
+        String gameDescription = "Answer 'yes' if the number is even, otherwise answer 'no'";
         Engine.startGame(gameDescription, questionsAndAnswers);
     }
 }
